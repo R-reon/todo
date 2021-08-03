92bd892d2c1d9ac78f3dbd110f5c2619c2e84574
ryo <NONE>
2021-08-04 05:19:20 +0900

    nemutai

--- myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('bbs/', include('bbs.urls')),
-    path('admin/', admin.site.urls),
-    path('', include('register.urls')),
-]
