3c2049dff6c3eeca33df9118c05978406ffa957a
ryo <NONE>
2021-11-03 22:13:25 +0900

    'todo'

---
+++ client/src/components/pages/HedgeHogs.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <v-btn @click="getHedgeHogData">これから、Todoを作ります。</v-btn>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Swal from "sweetalert2";
+import router from "../../router";
+export default {
+  name: "HedgeHogs",
+  mounted() {
+    this.checkLoggedIn();
+  },
+  methods: {
+    checkLoggedIn() {
+      this.$session.start();
+      if (!this.$session.has("token")) {
+        router.push("/auth");
+      }
+    },
+    getHedgeHogData() {
+      axios
+        .get("http://localhost:8000/api/hedgehogs/")
+        .then(res => {
+          console.log(res.data[0]);
+          console.log(res.data[1]);
+          console.log(res.data[2]);
+        })
+        .catch(e => {
+          Swal.fire({
+            type: "error",
+            title: "error",
+            text: "ERROR"
+          });
+        });
+    }
+  }
+};
+</script>
